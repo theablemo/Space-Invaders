4c88534a50f190884188bfb6477e8df158cb0188
Mohammad Abolneadian <mohammad1379_a@yahoo.com>
2020-05-29 16:02:22 +0430

    signup improve

--- src/Controllers/SignupPage.java
+++ src/Controllers/SignupPage.java
@@ -21,10 +21,25 @@ public class SignupPage {
     public void signupAction(MouseEvent mouseEvent) throws Exception {
         String usernameText = username.getText();
         String passwordText = password.getText();
+        if(usernameText.equals(""))
+        {
+            errorLabel.setText("Choose a username");
+            username.clear();
+            password.clear();
+            throw new Exception();
+        }
+        if(passwordText.equals(""))
+        {
+            errorLabel.setText("Choose a password");
+            username.clear();
+            password.clear();
+            throw new Exception();
+        }
         if(User.userExists(usernameText))
         {
             errorLabel.setText("This username Exists!");
-            errorLabel.setTextFill(Paint.valueOf("DD3022"));
+            username.clear();
+            password.clear();
             throw new Exception();
         }
         MainController.getInstance().makeUser(usernameText,passwordText);
